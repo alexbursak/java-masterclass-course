16b26ddeefb3bf5404b2d375cd29abf5b262dfad
alexbursak <alexbursak3@gmail.com>
2020-04-25 22:26:43 +0100

    lesson 22

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        int myNumber = (10 + 5) + (2 * 10);
+        System.out.println(myNumber);
     }
 }
