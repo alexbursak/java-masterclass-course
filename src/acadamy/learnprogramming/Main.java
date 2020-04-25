cf783fd96d59d94e26282a365a546df2b699a90c
alexbursak <alexbursak3@gmail.com>
2020-04-25 23:43:43 +0100

    lesson 25

--- src/acadamy/learnprogramming/Main.java
+++ src/acadamy/learnprogramming/Main.java
@@ -12,5 +12,18 @@ public class Main {
         System.out.println("Integer Max = " + myMaxIntValue);
         System.out.println("Busted Max = " + (myMaxIntValue + 1)); // overflow
         System.out.println("Busted Min = " + (myMinValue - 1)); // underflow
+
+        byte myMinByteValue = Byte.MIN_VALUE; // -128
+        byte myMixByteValue = Byte.MAX_VALUE; // 127
+
+        short myMinShortValue = Short.MIN_VALUE; // -32768
+        short myMaxShortValue = Short.MAX_VALUE; // 32767
+
+        long myLongNumber = 100L;
+        long myMinLongValue = Long.MIN_VALUE; // -9223372036854775808
+        long myMaxLongValue = Long.MAX_VALUE; // 9223372036854775807
+
+        System.out.println(myMinLongValue);
+        System.out.println(myMaxLongValue);
     }
 }
