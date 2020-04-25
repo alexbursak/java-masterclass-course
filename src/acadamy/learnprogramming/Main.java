795d431080a270fae2aa57ee0f8639c5ad8b579c
alexbursak <alexbursak3@gmail.com>
2020-04-26 00:01:37 +0100

    lesson 28

--- src/acadamy/learnprogramming/Main.java
+++ src/acadamy/learnprogramming/Main.java
@@ -3,30 +3,12 @@ package acadamy.learnprogramming;
 public class Main {
     public static void main(String[] args) {
 
-        int myValue = 1000;
+        int myInt = 5 / 3;
+        float myFloat = 5f / 3f;
+        double myDouble = 5d / 3d;
 
-        int myMinValue = Integer.MIN_VALUE;
-        int myMaxIntValue = Integer.MAX_VALUE;
-
-        System.out.println("Integer Min = " + myMinValue);
-        System.out.println("Integer Max = " + myMaxIntValue);
-        System.out.println("Busted Max = " + (myMaxIntValue + 1)); // overflow
-        System.out.println("Busted Min = " + (myMinValue - 1)); // underflow
-
-        byte myMinByteValue = Byte.MIN_VALUE; // -128
-        byte myMixByteValue = Byte.MAX_VALUE; // 127
-
-        short myMinShortValue = Short.MIN_VALUE; // -32768
-        short myMaxShortValue = Short.MAX_VALUE; // 32767
-
-        long myLongNumber = 100L;
-        long myMinLongValue = Long.MIN_VALUE; // -9223372036854775808
-        long myMaxLongValue = Long.MAX_VALUE; // 9223372036854775807
-
-        System.out.println(myMinLongValue);
-        System.out.println(myMaxLongValue);
-
-        int myIntNumber = 100;
-        byte myNewByteValue = (byte) myIntNumber;
+        System.out.println(myInt);
+        System.out.println(myFloat);
+        System.out.println(myDouble);
     }
 }
