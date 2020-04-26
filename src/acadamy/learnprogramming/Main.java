e384aaf594690c781ec81aa84ac3a37e68d24e2e
alexbursak <alexbursak3@gmail.com>
2020-04-26 12:20:44 +0100

    lesson 31

--- src/acadamy/learnprogramming/Main.java
+++ src/acadamy/learnprogramming/Main.java
@@ -4,12 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        char myChar = 'f';
-        char myUnicode = '\u0044';
-
-        System.out.println(myUnicode);
-
-        boolean myTrueBool = true;
-        boolean myFalseBool = false;
+        String myString = "Some string here";
+        System.out.println(myString);
     }
 }
