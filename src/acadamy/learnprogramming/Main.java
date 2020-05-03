49d364cc7390383f5beba5b8291d87a25031156f
alexbursak <alexbursak3@gmail.com>
2020-05-03 15:44:43 +0100

    lesson 62

--- src/acadamy/learnprogramming/Main.java
+++ src/acadamy/learnprogramming/Main.java
@@ -1,34 +1,28 @@
 package acadamy.learnprogramming;
 
 public class Main {
-
-    private static final String INVALID_VAL_MGS = "Invalid value!";
-
-    public static void main(String[] args) {
-        System.out.println(INVALID_VAL_MGS);
-    }
-
-    public static int calc(int p1, int p2) {
-        return p1 + p2;
-    }
-
-    public static double calc(double p1, double p2) {
-        return p1 + p2;
-    }
-
-    public static String calc(int p1) {
-        return "Num: " + p1;
-    }
-
-    public static String calc(String p1) {
-        return "String: " + p1;
-    }
-
-    public static String calc() {
-        return "Method";
-    }
-
-    public static void calc(double p1) {
-
+    int day = 4;
+    switch (day) {
+        case 1:
+            System.out.println("Monday");
+            break;
+        case 2:
+            System.out.println("Tuesday");
+            break;
+        case 3:
+            System.out.println("Wednesday");
+            break;
+        case 4:
+            System.out.println("Thursday");
+            break;
+        case 5:
+            System.out.println("Friday");
+            break;
+        case 6:
+            System.out.println("Saturday");
+            break;
+        case 7:
+            System.out.println("Sunday");
+            break;
     }
 }
