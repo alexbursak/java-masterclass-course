68a6b142f12e81613b6e748c1ee954b49681dfd6
alexbursak <alexbursak3@gmail.com>
2020-05-03 14:34:54 +0100

    lesson 58

--- src/acadamy/learnprogramming/Main.java
+++ src/acadamy/learnprogramming/Main.java
@@ -3,8 +3,30 @@ package acadamy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
+        calc();
+    }
 
-        String myString = "Some string here";
-        System.out.println(myString);
+    public static int calc(int p1, int p2) {
+        return p1 + p2;
+    }
+
+    public static double calc(double p1, double p2) {
+        return p1 + p2;
+    }
+
+    public static String calc(int p1) {
+        return "Num: " + p1;
+    }
+
+    public static String calc(String p1) {
+        return "String: " + p1;
+    }
+
+    public static String calc() {
+        return "Method";
+    }
+
+    public static void calc(double p1) {
+
     }
 }
